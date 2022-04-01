4fc27b01e87f5d5ce7504d1a19c93829b6b5e15d
Lunjalook <azra.cosickic@gmail.com>
2022-04-01 07:15:53 +0200

    Adding exercise 1 - Dividing

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,7 +1,7 @@
 function main(numOne, numTwo) {
 
     var quotient;
-    if (numTwo == 0) {
+    if (numTwo === 0) {
         quotient = 'Divide by 0! Cannot divide by zero!';
     } else {
         //only change code below this line
